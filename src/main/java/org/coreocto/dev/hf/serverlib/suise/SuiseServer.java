4b14da1dde76a9b6ed2a4472cb316526b8b9ebe1
coreocto <coreocto@hotmail.com>
2018-01-10 23:26:23 +0800

    Added dataProtected flag to allow searching on unencrypted data

--- src/main/java/org/coreocto/dev/hf/serverlib/suise/SuiseServer.java
+++ src/main/java/org/coreocto/dev/hf/serverlib/suise/SuiseServer.java
@@ -1,7 +1,7 @@
 package org.coreocto.dev.hf.serverlib.suise;
 
-import org.coreocto.dev.hf.commonlib.suise.bean.AddTokenResult;
-import org.coreocto.dev.hf.commonlib.suise.util.SuiseUtil;
+import org.coreocto.dev.hf.commonlib.sse.suise.bean.AddTokenResult;
+import org.coreocto.dev.hf.commonlib.sse.suise.util.SuiseUtil;
 import org.coreocto.dev.hf.commonlib.util.IBase64;
 import org.coreocto.dev.hf.commonlib.util.Registry;
 
@@ -18,6 +18,16 @@ public class SuiseServer {
     private Map<String, File> encFileList = null;
 
     private SuiseUtil suiseUtil = null;
+
+    public boolean isDataProtected() {
+        return dataProtected;
+    }
+
+    public void setDataProtected(boolean dataProtected) {
+        this.dataProtected = dataProtected;
+    }
+
+    private boolean dataProtected = true;
 
     public SuiseServer(SuiseUtil suiseUtil) {
         this.regularIdx = new HashMap<String, List<String>>();
@@ -68,30 +78,38 @@ public class SuiseServer {
                 String fileId = entry.getKey();
                 List<String> tmpList = entry.getValue();
 
-                int listSize = tmpList.size();
 
-                for (int i = 0; i < listSize; i++) {
-                    String ci = tmpList.get(i);
-
-                    // generate random 16 bytes
-                    byte[] randomBytes = new byte[16];
-                    suiseUtil.setRandomBytes(randomBytes, i);
-
-                    String randomVal = base64.encodeToString(randomBytes);
-
-                    byte[] srhTknBytes = null;
-
-                    try {
-                        srhTknBytes = base64.decodeToByteArray(searchToken);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                    // split the saved ci into li & ri
-                    byte[] li = suiseUtil.H(srhTknBytes, randomBytes);
-
-                    if ((base64.encodeToString(li) + randomVal).equals(ci)) {
+                if (dataProtected) {
+                    if (tmpList.contains(searchToken)) {
                         iw.add(fileId);
+                    }
+                } else {
+
+                    int listSize = tmpList.size();
+
+                    for (int i = 0; i < listSize; i++) {
+                        String ci = tmpList.get(i);
+
+                        // generate random 16 bytes
+                        byte[] randomBytes = new byte[16];
+                        suiseUtil.setRandomBytes(randomBytes, i);
+
+                        String randomVal = base64.encodeToString(randomBytes);
+
+                        byte[] srhTknBytes = null;
+
+                        try {
+                            srhTknBytes = base64.decodeToByteArray(searchToken);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        // split the saved ci into li & ri
+                        byte[] li = suiseUtil.H(srhTknBytes, randomBytes);
+
+                        if ((base64.encodeToString(li) + randomVal).equals(ci)) {
+                            iw.add(fileId);
+                        }
                     }
                 }
             }
